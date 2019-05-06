e1e261dec30f6a173be9731b829717c8220b00af
oat9002 <oat9002@hotmail.com>
2019-05-06 21:53:31 +0700

    Add payment log model

---
+++ server/model/PaymentLog.ts
@@ -0,0 +1,44 @@
+interface PaymentLog {
+    paymentId: string;
+    psId: string;
+    psForm: string;
+    void: string;
+    paymentControlId: string;
+    paymentPeriod: Date;
+    paymentPeriodItemId: string;
+    purchaseId: string;
+    expenseLog: string;
+    payroll: string;
+    paymentDate: Date;
+    calculatedPayroll: number;
+    calculatedPurchaseAmount: number;
+    calculatedPurchaseWithholding: number;
+    calculatedExpenseAmount: number;
+    expectedPaidAmount: number;
+    difference: number;
+    paidAmount: number;
+    withholdingAmount: number;
+    bankFee: number;
+    bankWithdrawAmount: number;
+    paymentType: PaymentType;
+    cheque: string;
+    proofOfPayment: string;
+    withholdingForm: string;
+    checkId: string;
+    txChequeId: string;
+    bankBatchRefNo: string;
+    bankRefNo: string;
+    bankTransaction: string;
+    paidTo: string;
+    expectedPayee: string;
+    enteredby: string;
+    verifiedby: string;
+    notes: string;
+    createdTime: Date;
+    batchPayments: string;
+    fiscalYear: Date;
+}
+
+type PaymentType = 'Direct Credit (DCT)'
+
+export default PaymentLog;
