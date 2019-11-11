541635a088b366319de5973152f487ba7a4e5ddd
oat9002 <oat9002@hotmail.com>
2019-11-12 01:37:50 +0700

    Fix

--- server/common/interfaces/MapHelper.ts
+++ server/common/interfaces/MapHelper.ts
@@ -1,12 +1,13 @@
 import PurchaseItem from "../../models/PurchanseItem";
 import { AirtableInsertRequest } from "../../services/interfaces/AirtableService";
 import PurchaseRequisition from "../../models/PurchaseRequisition";
+import InsertPurchaseItemsRequest from "../../models/requests/InsertPurchaseItemsRequest";
 
 interface MapHelper {
     mapAirtableRecToPurchaseItem(record: any): PurchaseItem;
     mapRequestToPurchaseItem(
-        request: PurchaseItem[]
-    ): AirtableInsertRequest<PurchaseItem>[];
+        request: InsertPurchaseItemsRequest
+    ): AirtableInsertRequest<PurchaseItem>;
     mapAirtableRecToPurchaseRequisition(record: any): PurchaseRequisition;
 }
 
