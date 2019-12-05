e65536e200e7e49c0b28b4a98b444a89e64e5659
oat9002 <oat9002@hotmail.com>
2019-12-05 23:44:00 +0700

    Refactor

--- client/src/components/modal/AddPurchaseItem.tsx
+++ client/src/components/modal/AddPurchaseItem.tsx
@@ -1,17 +1,10 @@
 import * as React from "react";
-import * as Yup from "yup";
-import {
-    Modal,
-    Alert,
-    Form,
-    Input,
-    Checkbox,
-    DatePicker,
-    InputNumber
-} from "antd";
+import { Modal, Alert } from "antd";
 import InsertPurchaseItemsRequest from "../../models/requests/InsertPurchaseItemsRequest";
 import { insertPurchaseItems } from "../../services/PurchseItemsService";
-import { getToday, removeTimeFromDate } from "../../services/Utils";
+import { removeTimeFromDate } from "../../services/Utils";
+import AddPurchaseItemForm from "../form/AddPurchaseItemForm/AddPurchaseItemForm";
+import { WrappedFormUtils } from "antd/lib/form/Form";
 
 export interface AddPurchaseItemProps {
     visible: boolean;
@@ -21,179 +14,46 @@ export interface AddPurchaseItemProps {
 const AddPurchaseItem: React.FC<AddPurchaseItemProps> = props => {
     const [isConfirmLoading, setIsConfirmLoading] = React.useState(false);
     const [isAlert, setIsAlert] = React.useState(false);
-    let request = createEmptyRequest();
+    const [formRef, setFormRef] = React.useState<WrappedFormUtils<any> | null>(
+        null
+    );
+
+    const saveFormRef = React.useCallback(node => {
+        if (node !== null) {
+            setFormRef(node);
+        }
+    }, []);
 
     const onCancel = () => {
         props.setVisible(false);
     };
 
     const onOk = async () => {
-        setIsConfirmLoading(true);
+        if (!!formRef) {
+            formRef.validateFields(async (err, values) => {
+                if (err) {
+                    setIsConfirmLoading(false);
+                    return;
+                }
 
-        if (!schema.isValidSync(request)) {
-            setIsConfirmLoading(false);
-            setIsAlert(true);
-            return;
-        }
+                setIsConfirmLoading(true);
 
-        const isSuccess = await insertPurchaseItems(
-            request as InsertPurchaseItemsRequest
-        );
+                const isSuccess = await insertPurchaseItems(mapRequest(values));
 
-        setIsConfirmLoading(false);
+                setIsConfirmLoading(false);
 
-        if (isSuccess) {
-            props.setVisible(false);
+                if (isSuccess) {
+                    props.setVisible(false);
+                } else {
+                    setIsAlert(true);
+                }
+
+                console.log("Received values of form: ", values);
+            });
         } else {
             setIsAlert(true);
         }
     };
-
-    const onRequestChange = (field: string) => (value: any) => {
-        const toUpdate = { ...request };
-        (toUpdate as any)[field] =
-            field === "paymentDueDate"
-                ? removeTimeFromDate(value.toDate())
-                : value;
-        request = toUpdate;
-    };
-
-    const ExtendedForm = Form.create({
-        name: "insert_pp",
-        onFieldsChange(_, changedField) {
-            const key = Object.keys(changedField)[0];
-            onRequestChange(changedField[key].name)(changedField[key].value);
-        }
-    })((props: any) => {
-        const { getFieldDecorator, getFieldError, isFieldTouched } = props.form;
-        const formItemLayout = {
-            labelCol: { span: 8 },
-            wrapperCol: { span: 16 }
-        };
-        const formatNumber = (value: string | number | undefined) =>
-            value ? `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",") : "";
-        const checkError = (field: string) =>
-            isFieldTouched(field) && getFieldError(field);
-
-        return (
-            <Form layout="horizontal">
-                <Form.Item
-                    label="Short Description"
-                    validateStatus={
-                        checkError("shortDescription") ? "error" : "success"
-                    }
-                    hasFeedback={checkError("shortDescription")}
-                    required
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator("shortDescription", {
-                        rules: [
-                            {
-                                required: true,
-                                message:
-                                    "Please fill in your short description!"
-                            }
-                        ]
-                    })(<Input />)}
-                </Form.Item>
-                <Form.Item label="Missing Receipt" {...formItemLayout}>
-                    {getFieldDecorator("missingReceipt")(<Checkbox />)}
-                </Form.Item>
-                <Form.Item
-                    label="Payment Due Date"
-                    validateStatus={
-                        checkError("paymentDueDate") ? "error" : "success"
-                    }
-                    hasFeedback={checkError("paymentDueDate")}
-                    required
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator("paymentDueDate", {
-                        rules: [
-                            {
-                                required: true,
-                                message: "Please fill in your payment due date!"
-                            }
-                        ]
-                    })(<DatePicker />)}
-                </Form.Item>
-                <Form.Item label="USD Invoice Amount" {...formItemLayout}>
-                    {getFieldDecorator("usdInvoiceAmount")(
-                        <InputNumber min={0} formatter={formatNumber} />
-                    )}
-                </Form.Item>
-                <Form.Item label="THB Invoice Amount" {...formItemLayout}>
-                    {getFieldDecorator("thbInvoiceAmount")(
-                        <InputNumber min={0} formatter={formatNumber} />
-                    )}
-                </Form.Item>
-                <Form.Item
-                    label="Payment Amount"
-                    validateStatus={
-                        checkError("paymentAmount") ? "error" : "success"
-                    }
-                    hasFeedback={checkError("paymentAmount")}
-                    required
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator("paymentAmount", {
-                        rules: [
-                            {
-                                required: true,
-                                message: "Please fill in your payment amount!"
-                            }
-                        ]
-                    })(<InputNumber min={0} formatter={formatNumber} />)}
-                </Form.Item>
-                <Form.Item
-                    label="Request Justification"
-                    validateStatus={
-                        checkError("requestJustification") ? "error" : "success"
-                    }
-                    hasFeedback={checkError("requestJustification")}
-                    required
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator("requestJustification", {
-                        rules: [
-                            {
-                                required: true,
-                                message:
-                                    "Please fill in your requested justification!"
-                            }
-                        ]
-                    })(<Input />)}
-                </Form.Item>
-                {/* <Form.Item label="Entered by">
-                            <InputWithLabel
-                                onChangeWithUpdate={onRequestChange("enteredBy")}
-                                value={request.enteredBy}
-                            />
-                        </Form.Item> */}
-                <Form.Item
-                    label="Account Payable"
-                    validateStatus={
-                        checkError("accountPayable") ? "error" : "success"
-                    }
-                    hasFeedback={checkError("accountPayable")}
-                    required
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator("accountPayable", {
-                        rules: [
-                            {
-                                required: true,
-                                message: "Please fill in your account payable!"
-                            }
-                        ]
-                    })(<Input />)}
-                </Form.Item>
-                <Form.Item label="Suplier" {...formItemLayout}>
-                    {getFieldDecorator("supplier")(<Input />)}
-                </Form.Item>
-            </Form>
-        );
-    });
 
     return (
         <Modal
@@ -208,56 +68,31 @@ const AddPurchaseItem: React.FC<AddPurchaseItemProps> = props => {
             {isAlert ? (
                 <Alert
                     type="error"
-                    message="Insert failed"
+                    message="Error, problems have been occured"
                     banner
                     closable
                     onClose={() => setIsAlert(false)}
                 />
             ) : null}
-            <ExtendedForm wrappedComponentRef />
+            <AddPurchaseItemForm ref={saveFormRef} />
         </Modal>
     );
 };
 
-function createEmptyRequest(): InsertPurchaseItemsRequest {
+function mapRequest(data: any): InsertPurchaseItemsRequest {
     return {
-        shortDescription: "undefined",
-        missingReceipt: undefined,
-        paymentDueDate: getToday(),
-        usdInvoiceAmount: undefined,
-        thbInvoiceAmount: undefined,
-        paymentAmount: 0,
-        requestJustification: "",
+        shortDescription: data.shortDescription,
+        missingReceipt: data.missingReceipt,
+        paymentDueDate: removeTimeFromDate(data.paymentDueDate.toDate()),
+        usdInvoiceAmount: data.usdInvoiceAmount,
+        thbInvoiceAmount: data.thbInvoiceAmount,
+        paymentAmount: data.paymentAmount,
+        requestJustification: data.requestJustification,
         enteredBy: undefined,
-        accountPayable: "",
-        supplier: undefined,
+        accountPayable: data.accountPayable,
+        supplier: data.supplier,
         reviewedBy: undefined
     };
 }
-
-const schema = Yup.object({
-    shortDescription: Yup.string()
-        .trim()
-        .min(1)
-        .required(),
-    missingReceipt: Yup.boolean().notRequired(),
-    paymentDueDate: Yup.date().required(),
-    usdInvoiceAmount: Yup.number().notRequired(),
-    thbInvoiceAmount: Yup.number().notRequired(),
-    paymentAmount: Yup.number()
-        .min(0)
-        .required(),
-    requestJustification: Yup.string()
-        .trim()
-        .min(1)
-        .required(),
-    enteredBy: Yup.object(),
-    accountPayable: Yup.string()
-        .trim()
-        .min(1)
-        .required(),
-    supplier: Yup.string().notRequired(),
-    reviewedBy: Yup.object()
-});
 
 export default AddPurchaseItem;
